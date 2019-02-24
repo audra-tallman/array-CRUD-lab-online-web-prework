30f93a40df8978e9e364e31c3bcfe8a37a7633c9
Audra Tallman <audra.tallman@hotmail.com>
2019-02-24 22:30:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,10 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  berries = ["blue berries", "strawberries", "raspberries", "black berries"]
 end
 
 def add_element_to_end_of_array(array, element)
+  berries.
   
 end
 
