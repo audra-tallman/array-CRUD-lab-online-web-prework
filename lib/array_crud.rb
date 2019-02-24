4fc9414612037310edb4eab58e1af9222c4c9286
Audra Tallman <audra.tallman@hotmail.com>
2019-02-24 22:34:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  berries.
-  
+  berries << "cherries"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  berries.pop
 end
 
 def remove_element_from_end_of_array(array)
