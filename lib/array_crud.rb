c80cb9ed02d7c38c72a7d4110ea6c2f9fab32cfe
Audra Tallman <audra.tallman@hotmail.com>
2019-02-24 22:42:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,33 +3,33 @@ def create_an_empty_array
 end
 
 def create_an_array
-  berries = ["blueberries", "strawberries", "raspberries", "blackberries"]
+  ["blueberries", "strawberries", "raspberries", "blackberries"]
 end
 
 def add_element_to_end_of_array(array, element)
-  berries << "cranberries"
+  array << "cranberries"
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array = berries.unshift "boysenberries"
+  array.unshift "boysenberries"
 end
 
 def remove_element_from_end_of_array(array)
- remove_element_from_end_of_array = berries.pop
+ array.pop
 end
 
 def remove_element_from_start_of_array(array)
- remove_element_from_start_of_array = berries.shift
+ array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_element_from_index = berries [2]
+  array [2]
 end
 
 def retrieve_first_element_from_array(array)
-  retrieve_first_element_from_array = berries.first
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  retrieve_last_element_from_array = berries.last
+  array.last
 end
