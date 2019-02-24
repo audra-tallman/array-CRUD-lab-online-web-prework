a00a8fb4471561ce35c93a7574659485e682a5d9
Audra Tallman <audra.tallman@hotmail.com>
2019-02-24 22:40:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array = berries << "cranberries"
+  berries << "cranberries"
 end
 
 def add_element_to_start_of_array(array, element)
